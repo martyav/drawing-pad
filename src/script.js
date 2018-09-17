15c2355d61ffb71a2c91d1976e38a09fd7d2384d
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-09-17 01:13:00 -0400

    wired up some basic event handling

--- src/script.js
+++ src/script.js
@@ -1,26 +1,35 @@
-function setUpCanvas(canvas, frame, context) {
-  canvas.width = frame.width;
-  canvas.height = frame.height;
-
+function setCanvasProperties(canvas, frame, context) {
   context.strokeStyle = document.body.style.getPropertyValue('--color');
   context.lineWidth = document.body.style.getPropertyValue('--width');
   context.lineCap = document.body.style.getPropertyValue('--cap');;
   context.lineJoin = document.body.style.getPropertyValue('--join');
 
-  console.log("set up canvas ran");
+  console.log("set canvas ran");
 }
 
-function draw() {}
+function draw(event) {
+  console.log('drawing');
+  console.log(event);
+}
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.setProperty("--color", "#000000");
+function handleUpdate() {
+  document.documentElement.style.setProperty(`--${this.name}`, this.value);
+}
+
+document.addEventListener("DOMContentLoaded", () => {  
+  document.body.style.setProperty("--color", "#f00");
   document.body.style.setProperty('--width', '10');
   document.body.style.setProperty('--cap', 'round');
   document.body.style.setProperty('--join', 'round');
-
-  const canvas = document.querySelector("canvas");
-  const frame = document.getElementById("frame");
-  const context = canvas.getContext("2d", { alpha: false }); // setting alpha to false optimizes rendering
-
-  setUpCanvas(canvas, frame, context);
+  
+  const canvas = document.querySelector("#drawHere");
+  const frame = document.getElementById("#frame");
+  const context = canvas.getContext("2d"); // setting alpha to false optimizes rendering...but then the canvas bg is rendered black, and changing the color is quite slow
+  const inputs = Array.from(document.querySelectorAll(`.controls input`));
+  
+  setCanvasProperties(canvas, frame, context);
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mousedown', draw);
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  inputs.forEach(input => input.addEventListener('change', handleUpdate));
 });
