6bfb02e0c305cf62cd88f7f8af702efd6ecf1cee
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-09-16 22:40:21 -0400

    learned how to access and change css variables from javascript file

--- src/script.js
+++ src/script.js
@@ -2,16 +2,22 @@ function setUpCanvas(canvas, frame, context) {
   canvas.width = frame.width;
   canvas.height = frame.height;
 
-  // These will be interacting with css selectors?
-  context.strokeStyle = "#000000";
-  context.lineWidth = 10;
-  context.lineJoin = "round";
-  context.lineCap = "round";
+  context.strokeStyle = document.body.style.getPropertyValue('--color');
+  context.lineWidth = document.body.style.getPropertyValue('--width');
+  context.lineCap = document.body.style.getPropertyValue('--cap');;
+  context.lineJoin = document.body.style.getPropertyValue('--join');
 
-  console.log('set up canvas ran');
+  console.log("set up canvas ran");
 }
 
+function draw() {}
+
 document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.setProperty("--color", "#000000");
+  document.body.style.setProperty('--width', '10');
+  document.body.style.setProperty('--cap', 'round');
+  document.body.style.setProperty('--join', 'round');
+
   const canvas = document.querySelector("canvas");
   const frame = document.getElementById("frame");
   const context = canvas.getContext("2d", { alpha: false }); // setting alpha to false optimizes rendering
