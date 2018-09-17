640e282a2dd2e558cd96fe29b13faa00f75d99b2
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-09-17 01:47:38 -0400

    got drawing on canvas working

--- src/script.js
+++ src/script.js
@@ -8,8 +8,28 @@ function setCanvasProperties(canvas, frame, context) {
 }
 
 function draw(event) {
+  const isDrawing = document.body.style.getPropertyValue('--isDrawing') === 'true' // we do this weird conversion because the value we get back is a string containing either the word true or false!
+  console.log(isDrawing);
+  
+  if (!isDrawing) {
+    console.log('isDrawing is false')
+    return;
+  }
+
+  const localCanvas = document.querySelector("#drawHere");
+  const localContext = localCanvas.getContext("2d");
+  const localLastX = document.body.style.getPropertyValue('--lastX');
+  const localLastY = document.body.style.getPropertyValue('--lastY');
+
+  localContext.beginPath();
+  localContext.moveTo(localLastX, localLastY);
+  localContext.lineTo(event.offsetX, event.offsetY);
+  localContext.lineWidth = 10;
+  localContext.stroke();
+  document.body.style.setProperty('--lastX', event.offsetX);
+  document.body.style.setProperty('--lastY', event.offsetY);
+
   console.log('drawing');
-  console.log(event);
 }
 
 function handleUpdate() {
@@ -21,6 +41,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.style.setProperty('--width', '10');
   document.body.style.setProperty('--cap', 'round');
   document.body.style.setProperty('--join', 'round');
+  document.body.style.setProperty('--isDrawing', false); // Note: this method converts the second arg to a string
+  document.body.style.setProperty('--lastX', 0);
+  document.body.style.setProperty('--lastY', 0);
   
   const canvas = document.querySelector("#drawHere");
   const frame = document.getElementById("#frame");
@@ -29,7 +52,18 @@ document.addEventListener("DOMContentLoaded", () => {
   
   setCanvasProperties(canvas, frame, context);
   canvas.addEventListener('mousemove', draw);
-  canvas.addEventListener('mousedown', draw);
+  canvas.addEventListener('mousedown', () => {
+    document.body.style.setProperty('--isDrawing', true);
+    document.body.style.setProperty('--lastX', event.offsetX);
+    document.body.style.setProperty('--lastY', event.offsetY);
+  });
+  canvas.addEventListener('mouseup', () => {
+    document.body.style.setProperty('--isDrawing', false)
+  });
+  canvas.addEventListener('mouseout', () => {
+    document.body.style.setProperty('--isDrawing', false)
+  });
+
   inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
   inputs.forEach(input => input.addEventListener('change', handleUpdate));
 });
