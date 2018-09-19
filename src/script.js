6acf22f6cd7a88327233d9371740b1de0030ff1b
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-09-18 22:43:06 -0400

    refactoring script file

--- src/script.js
+++ src/script.js
@@ -1,17 +1,5 @@
-function setCanvasProperties(canvas, context) {
-    // Width/height is set in this way here bc naively using css to set it simply causes the canvas to stretch without redefining the number of pixels, resulting in stroke drawing that is far off from the cursor. See https://stackoverflow.com/questions/10214873/make-canvas-as-wide-and-as-high-as-parent and https://stackoverflow.com/questions/11060110/drawing-at-cursor-position-on-canvas-with-javascript 
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-
-    context.strokeStyle = document.body.style.getPropertyValue('--color');
-    context.lineWidth = document.body.style.getPropertyValue('--width');
-    context.lineCap = document.body.style.getPropertyValue('--nib');;
-
-    console.log("set canvas ran");
-}
-
 function draw(event) {
-    const isDrawing = document.body.style.getPropertyValue('--isDrawing') === 'true' // we do this weird conversion because the value we get back is a string containing either the word true or false!
+    const isDrawing = JSON.parse(document.body.style.getPropertyValue('--isDrawing')); // we do this weird conversion because the value we get back is a string containing either the word true or false!
 
     if (!isDrawing) {
         return;
@@ -21,8 +9,6 @@ function draw(event) {
     const localContext = localCanvas.getContext("2d");
     const localLastX = document.body.style.getPropertyValue('--lastX');
     const localLastY = document.body.style.getPropertyValue('--lastY');
-    console.log(localLastX);
-    console.log(localLastY);
 
     localContext.beginPath();
     localContext.moveTo(localLastX, localLastY);
@@ -30,12 +16,9 @@ function draw(event) {
     localContext.stroke();
     document.body.style.setProperty('--lastX', event.offsetX);
     document.body.style.setProperty('--lastY', event.offsetY);
-
-    console.log('drawing');
 }
 
 function handleUpdate() {
-    console.log('handleUpdate was triggered');
     const property = `--${this.name}`;
     document.body.style.setProperty(property, this.value);
 
@@ -43,50 +26,49 @@ function handleUpdate() {
     const localContext = localCanvas.getContext("2d");
     const changedValue = document.body.style.getPropertyValue(property);
 
-    switch (this.name) {
-        case 'color':
+    switch (property) {
+        case '--color':
             localContext.strokeStyle = changedValue;
             break;
-        case 'width':
+        case '--width':
             localContext.lineWidth = changedValue;
             break;
-        case 'nib':
+        case '--nib':
             localContext.lineCap = changedValue;
             break;
     }
 }
 
 function eraseAll() {
-    console.log('Erase all was clicked');
     const localCanvas = document.querySelector("#drawHere");
     const localContext = localCanvas.getContext("2d");
 
     localContext.clearRect(0, 0, localCanvas.width, localCanvas.height);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const colorInput = document.querySelector('[name=color]').value;
-    const widthInput = document.querySelector('[name=width]').value;
-    const nibInput = document.querySelector('[name=nib]').value;
-
-
+function setCSSVariables(colorInput, widthInput, nibInput) {
     document.body.style.setProperty("--color", `${ colorInput }`);
     document.body.style.setProperty('--width', `${ widthInput }`);
     document.body.style.setProperty('--nib', `${ nibInput }`);
     document.body.style.setProperty('--isDrawing', false); // Note: this method converts the second arg to a string
     document.body.style.setProperty('--lastX', 0);
     document.body.style.setProperty('--lastY', 0);
+}
 
-    const canvas = document.querySelector("#drawHere");
-    const context = canvas.getContext("2d"); // setting alpha to false optimizes rendering...but then the canvas bg is rendered black, and changing the color is quite slow
-    const inputs = Array.from(document.querySelectorAll(`#controls input`));
-    const nibMenu = document.querySelector('[name=nib]');
-    const eraseAllButton = document.querySelector('#eraseAll');
+function setCanvasProperties(canvas, context) {
+    // Width/height is set in this way here bc naively using css to set it simply causes the canvas to stretch without redefining the number of pixels, resulting in stroke drawing that is far off from the cursor. See https://stackoverflow.com/questions/10214873/make-canvas-as-wide-and-as-high-as-parent and https://stackoverflow.com/questions/11060110/drawing-at-cursor-position-on-canvas-with-javascript 
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
 
-    setCanvasProperties(canvas, context);
+    context.strokeStyle = document.body.style.getPropertyValue('--color');
+    context.lineWidth = document.body.style.getPropertyValue('--width');
+    context.lineCap = document.body.style.getPropertyValue('--nib');;
+}
 
+function addEventHandlers(canvas, inputs, nibMenu, eraseAllButton) {
     canvas.addEventListener('mousemove', draw);
     canvas.addEventListener('mousedown', () => {
+        document.body.style.setProperty('--isDrawing', true);
         document.body.style.setProperty('--lastX', event.offsetX);
         document.body.style.setProperty('--lastY', event.offsetY);
         document.body.style.setProperty('--isDrawing', true);
@@ -101,4 +83,20 @@ document.addEventListener("DOMContentLoaded", () => {
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     nibMenu.addEventListener('change', handleUpdate);
     eraseAllButton.addEventListener('click', eraseAll);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.querySelector("#drawHere");
+    const context = canvas.getContext("2d"); // setting alpha to false optimizes rendering...but then the canvas bg is rendered black, and changing the color is quite slow
+    const inputs = Array.from(document.querySelectorAll(`#controls input`));
+    const nibMenu = document.querySelector('[name=nib]');
+    const eraseAllButton = document.querySelector('#eraseAll');
+
+    const colorInput = document.querySelector('[name=color]').value;
+    const widthInput = document.querySelector('[name=width]').value;
+    const nibInput = nibMenu.value;
+
+    setCSSVariables(colorInput, widthInput, nibInput);
+    setCanvasProperties(canvas, context);
+    addEventHandlers(canvas, inputs, nibMenu, eraseAllButton);
 });
